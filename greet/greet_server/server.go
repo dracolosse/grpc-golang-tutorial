a51fbb850aabf85a4a1ab8034439daefbaafcf6d
Truc NGUYEN <nguyentt.dev@gmail.com>
2020-04-20 20:30:00 +0200

    add part 3 exercise

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"go-grpc-tutorial/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -29,6 +32,41 @@ func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, streamRes gree
 		res := greetpb.GreetManyTimesResponse{Result: greeting}
 		streamRes.Send(&res)
 		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
+/**
+For demonstration purpose, you should create an file named test.txt in the current directory.
+The size file should be large to let you check at the client side.
+ */
+func (*server) DownloadFile(req *greetpb.FileRequest, streamRes greetpb.GreetService_DownloadFileServer) error  {
+	chunkSize := 64 * 1024 // 64kiB recommended chunk size
+	absPath, _ := filepath.Abs("../go-grpc-tutorial/greet/greet_server")
+	file, err := os.Open(absPath + "/" + req.GetFileName()) // only test.txt accepted here.
+	if err != nil {
+		fmt.Printf("File not found or err: %v", err)
+		return err
+	}
+	defer file.Close()
+
+	buff := make([]byte, chunkSize)
+	for {
+		bytesRead, err := file.Read(buff)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+		resp := greetpb.Chunk{
+			Chunk: buff[:bytesRead],
+		}
+
+		if err = streamRes.Send(&resp); err != nil {
+			log.Println("error while sending chunk:", err)
+			return err
+		}
 	}
 	return nil
 }
